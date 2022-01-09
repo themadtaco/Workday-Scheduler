bb38975bf3a23f04d86357d0aed2690b2fdf1383
Drogon <aatrox48@gmail.com>
2022-01-09 17:04:53 -0600

    added function to change textarea color according to current time

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,7 +1,37 @@
 // Variables
 var DateTime = luxon.DateTime;
 
+// Setting current date to readable format
 var now = DateTime.now().toLocaleString();
 
 // Setting current date to header
 $("p#currentDay").html(now);
+
+// Function to check time blocks and change classes accordingly
+var checkTime = function() {
+    // Get current hour
+    var currentHour = DateTime.now().hour;
+
+    // Loop over time blocks
+    $(".description").each(function() {
+        var timeBlockHour = parseInt($(this).attr("id"));
+
+
+        // Compare hour and add appropriate classes
+        if (timeBlockHour < currentHour){
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        } else if (timeBlockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    });
+};
+
+checkTime();
